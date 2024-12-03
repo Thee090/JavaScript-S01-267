1e90624e711843b8f2ff2bed30b2e17c34333da7
Thee090 <theerachot090218@gmail.com>
2024-12-03 11:27:56 +0700

    chapter2

---
+++ Chapter2/1-funtion.js
@@ -0,0 +1,11 @@
+// 1. Funtion Definition
+function printHello(name) {
+    console.log("Hello " +name);
+    return name + "hello!";
+}
+
+console.log(printHello.name);
+
+let result = printHello("Theerachot !");
+console.log(result);
+
---
+++ Chapter2/strsplit.js
@@ -0,0 +1,7 @@
+const str = 'The quock brrow fox jump over the lay dog.';
+
+const words = str.split(' ');
+console.log(word[3]);
+
+const chars = str.split('');
+console.log(char[8]);
